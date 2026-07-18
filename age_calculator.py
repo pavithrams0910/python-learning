1efeeedb65208e2c5f8aafedb3580d4a3405cfb3
PAVITHRA M <pavithrams0910@gmail.com>
2026-07-18 21:22:40 +0530

    Add my first Python program

---
+++ age_calculator.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+birth_year = int(input("Enter your birth year: "))
+
+age = 2025 - birth_year
+
+print("----------------")
+print("Name:", name)
+print("Age:", age)
+print("Status: Student")
