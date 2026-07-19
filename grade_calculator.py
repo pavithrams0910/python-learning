a8344ec170b2ffb1d73063ed0fa6bf232ffd850d
PAVITHRA M <pavithrams0910@gmail.com>
2026-07-19 15:03:08 +0530

    Add files via upload

---
+++ grade_calculator.py
@@ -0,0 +1,11 @@
+marks=int(input("enter the marks"))
+if marks >100 or marks<0:
+    print("invalid")
+elif marks>=90:
+    print("grade A")
+elif marks>=75:
+    print("grade B")
+elif marks >=50:
+    print("grade C")
+else:
+        print("fail")
