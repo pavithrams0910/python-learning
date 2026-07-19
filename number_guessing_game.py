acf7db6515426c9de074f45221ff03ab0d2fbb15
PAVITHRA M <pavithrams0910@gmail.com>
2026-07-19 15:29:22 +0530

    Add files via upload

---
+++ number_guessing_game.py
@@ -0,0 +1,6 @@
+secret=7
+guess=int(input("guess anumber blw 1 and 10:"))
+if guess==secret:
+    print("congrats! you guessed correctly,")
+else:
+    print("wrong guess.the correct number was",secret)
